4d2615b6a1da23882f5b104af0474865ca707640
Aiden Cary <ACARY@cub.uca.edu>
2025-04-08 15:57:59 -0500

    Created Strategy design pattern to be implemented for all filters

---
+++ src/Filter/PokemonFilterStrategy.java
@@ -0,0 +1,8 @@
+package GUI;
+
+import Data.Pokemon;
+
+// Interface for implementing Strategy design pattern for filtering
+public interface PokemonFilterStrategy {
+    boolean filter(Pokemon p);
+}
---
+++ src/Filter/TypeFilter.java
@@ -0,0 +1,4 @@
+package Filter;
+
+public interface TypeFilter {
+}
